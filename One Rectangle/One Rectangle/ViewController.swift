847e2c9aa277a42985b40cf805912cfc78249ccb
Sinuhé Ruedas <sinuar@Sinuars-MacBook-Pro.local>
2021-12-03 18:05:38 -0600

    Creación de rectángulo

---
+++ One Rectangle/One Rectangle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  One Rectangle
+//
+//  Created by Sinuhé Ruedas on 03/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var rectanImage: UIImageView!
+    
+}
+
